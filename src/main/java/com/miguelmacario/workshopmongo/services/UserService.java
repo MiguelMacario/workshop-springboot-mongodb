35aa15532477ec8ee1f78be55dff8e81625f10e1
Miguel <miguel251028@gmail.com>
2025-01-28 20:54:02 -0300

    User insertion with POST

--- src/main/java/com/miguelmacario/workshopmongo/services/UserService.java
+++ src/main/java/com/miguelmacario/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.miguelmacario.workshopmongo.services;
 
 import com.miguelmacario.workshopmongo.domain.User;
+import com.miguelmacario.workshopmongo.dto.UserDTO;
 import com.miguelmacario.workshopmongo.repository.UserRepository;
 import com.miguelmacario.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,15 @@ public class UserService {
 
     public User findById(String id) {
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto nao econtrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+
     }
 
 }
