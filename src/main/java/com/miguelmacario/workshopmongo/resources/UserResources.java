f89d569dde810c3ba01d58b4851fab10790d6ad2
Miguel <miguel251028@gmail.com>
2025-01-29 18:25:27 -0300

    Update user with PUT

--- src/main/java/com/miguelmacario/workshopmongo/resources/UserResources.java
+++ src/main/java/com/miguelmacario/workshopmongo/resources/UserResources.java
@@ -46,4 +46,12 @@ public class UserResources {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = userService.fromDTO(objDto);
+        obj.setId(id);
+        obj = userService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 }
