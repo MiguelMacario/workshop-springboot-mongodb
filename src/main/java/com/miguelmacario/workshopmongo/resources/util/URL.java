735d38a1a14c935338f427714cbfb1a31f2e939a
Miguel <miguel251028@gmail.com>
2025-01-30 19:27:54 -0300

    Query with multiple criteria

--- src/main/java/com/miguelmacario/workshopmongo/resources/util/URL.java
+++ src/main/java/com/miguelmacario/workshopmongo/resources/util/URL.java
@@ -2,11 +2,24 @@ package com.miguelmacario.workshopmongo.resources.util;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class URL {
 
     public static String decodeParam(String str) {
         return URLDecoder.decode(str, StandardCharsets.UTF_8);
+    }
+
+    public static LocalDate convertDate(String textDate, LocalDate defaultDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try{
+            return LocalDate.parse(textDate, formatter);
+        } catch (DateTimeParseException e){
+            return defaultDate;
+        }
     }
 
 }
